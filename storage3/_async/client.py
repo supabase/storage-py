838af7c0aded3c2e20df2ddd8e665da33d65106d
Dani Reinón <dani@dribo.es>
2022-04-29 00:49:19 +0200

    feat: add key to clients

--- storage3/_async/client.py
+++ storage3/_async/client.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from ..utils import AsyncClient, __version__
 from .bucket import AsyncStorageBucketAPI
 from .file_api import AsyncBucketProxy
@@ -10,10 +11,14 @@ __all__ = [
 class AsyncStorageClient(AsyncStorageBucketAPI):
     """Manage storage buckets and files."""
 
-    def __init__(self, url: str, headers: dict[str, str]) -> None:
+    def __init__(self, url: str, key: str, headers: dict[str, str] = None) -> None:
         super().__init__(
             url,
-            {"User-Agent": f"supabase-py/storage3 v{__version__}", **headers},
+            {
+                "User-Agent": f"supabase-py/storage3 v{__version__}",
+                **self._get_auth_headers(key),
+                **(headers or {}),
+            },
             AsyncClient(),
         )
 
@@ -26,3 +31,12 @@ class AsyncStorageClient(AsyncStorageBucketAPI):
             The unique identifier of the bucket
         """
         return AsyncBucketProxy(id, self.url, self.headers, self._client)
+
+    @staticmethod
+    def _get_auth_headers(key: str) -> dict[str, str]:
+        """Helper method to get auth headers."""
+        # What's the corresponding method to get the token
+        return {
+            "apiKey": key,
+            "Authorization": f"Bearer {key}",
+        }
--- storage3/_sync/client.py
+++ storage3/_sync/client.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from ..utils import SyncClient, __version__
 from .bucket import SyncStorageBucketAPI
 from .file_api import SyncBucketProxy
@@ -10,10 +11,14 @@ __all__ = [
 class SyncStorageClient(SyncStorageBucketAPI):
     """Manage storage buckets and files."""
 
-    def __init__(self, url: str, headers: dict[str, str]) -> None:
+    def __init__(self, url: str, key: str, headers: dict[str, str] = None) -> None:
         super().__init__(
             url,
-            {"User-Agent": f"supabase-py/storage3 v{__version__}", **headers},
+            {
+                "User-Agent": f"supabase-py/storage3 v{__version__}",
+                **self._get_auth_headers(key),
+                **(headers or {}),
+            },
             SyncClient(),
         )
 
@@ -26,3 +31,12 @@ class SyncStorageClient(SyncStorageBucketAPI):
             The unique identifier of the bucket
         """
         return SyncBucketProxy(id, self.url, self.headers, self._client)
+
+    @staticmethod
+    def _get_auth_headers(key: str) -> dict[str, str]:
+        """Helper method to get auth headers."""
+        # What's the corresponding method to get the token
+        return {
+            "apiKey": key,
+            "Authorization": f"Bearer {key}",
+        }
