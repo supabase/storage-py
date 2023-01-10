216cf3667479380fb893e16b4bd47d31f5a2b641
joel@joellee.org <joel@joellee.org>
2023-01-10 22:16:59 +0800

    fix: remove stray /

--- storage3/_sync/file_api.py
+++ storage3/_sync/file_api.py
@@ -85,7 +85,7 @@ class SyncBucketActionsMixin:
             "render/image" if options.get("transform") else "object"
         )
         transformation_query = urllib.parse.urlencode(options)
-        query_string = f"/?{transformation_query}" if transformation_query else ""
+        query_string = f"?{transformation_query}" if transformation_query else ""
         _path = self._get_final_path(path)
         return f"{self._client.base_url}{render_path}/public{_path}{query_string}"
 
