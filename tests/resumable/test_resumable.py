e13466b78b586b53a249001e4619f51792a69588
Juan Carlos <juancarlospaco@gmail.com>
2024-10-14 22:38:10 -0300

    fix: test resumable WIP

---
+++ tests/resumable/test_resumable.py
@@ -0,0 +1,56 @@
+import os
+
+
+def test_sync_client(sync_client, file, test_bucket):
+    client = sync_client
+
+    """Check file was created during configuration phase"""
+    assert file is not None
+
+    """Verify test_bucket is not an empty string"""
+    assert len(test_bucket.strip()) > 0
+
+    client.resumable.create_unique_link(bucketname=test_bucket, filename=file.name)
+    link = client.resumable.get_link(file.name)
+
+    """Verify the link was generated as expected"""
+    assert len(link) > 0
+
+    """Check the file is not empty"""
+    assert os.stat(file.name).st_size > 0
+
+    """Verify if the file was loaded correctly"""
+    client.resumable.upload(file.name)
+    bucket = client.get_bucket(test_bucket)
+    file_loaded = list(filter(lambda e: e["name"] == file.name, bucket.list()))
+    assert len(file_loaded) == 1
+
+    bucket.remove(file.name)
+
+
+def test_deferred_sync_client(sync_client, file, test_bucket):
+
+    client = sync_client
+
+    """Check file was created during configuration phase"""
+    assert file is not None
+
+    """Verify test_bucket is not an empty string"""
+    assert len(test_bucket.strip()) > 0
+
+    client.resumable.create_unique_link(bucketname=test_bucket, objectname=file.name)
+    link = client.resumable.get_link(file.name)
+
+    """Verify the link was generated as expected"""
+    assert len(link) > 0
+
+    """Check the file is not empty"""
+    assert os.stat(file.name).st_size > 0
+
+    """Verify if the file was loaded correctly"""
+    client.resumable.upload(file.name, mb_size=10, upload_defer=True, link=link, objectname=file.name)
+    bucket = client.get_bucket(test_bucket)
+    file_loaded = list(filter(lambda e: e["name"] == file.name, bucket.list()))
+    assert len(file_loaded) == 1
+
+    bucket.remove(file.name)
