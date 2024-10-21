3925e9dbadce573cc06b72ae9fad65324888fb47
Juan Carlos <juancarlospaco@gmail.com>
2024-10-21 17:02:16 -0300

    fix: Style

--- storage3/_async/resumable.py
+++ storage3/_async/resumable.py
@@ -5,7 +5,8 @@ from datetime import datetime
 from ..types import FileInfo, UploadMetadata
 from ..utils import AsyncClient, FileStore, StorageException
 
-__all__ = ["AsyncResumableUpload"]
+
+__all__ = ("AsyncResumableUpload", )
 
 
 class AsyncResumableUpload:
